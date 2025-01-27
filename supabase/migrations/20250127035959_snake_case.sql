4cf2b7daf96f33c6544a23b5d9b7e1eca4024632
Nam Hoang Le <hoangnamb1.288@gmail.com>
2025-01-27 14:34:21 +0700

    Correct migration SQLs

--- supabase/migrations/20250127035959_snake_case.sql
+++ supabase/migrations/20250127035959_snake_case.sql
@@ -1,110 +1,62 @@
+drop function if exists "public"."update_updatedat_column"();
+
 alter table "public"."bill_members" drop constraint "bill_members_userId_fkey";
+alter table "public"."bill_members" drop constraint "bill_members_bill_id_fkey";
 
 alter table "public"."bills" drop constraint "bills_creatorId_fkey";
-
 alter table "public"."bills" drop constraint "bills_updaterId_fkey";
 
 alter table "public"."notifications" drop constraint "notifications_billId_fkey";
-
+alter table "public"."notifications" drop constraint "notifications_userId_fkey";
 alter table "public"."notifications" drop constraint "notifications_triggerId_fkey";
 
-alter table "public"."notifications" drop constraint "notifications_userId_fkey";
+-- bill_members
 
-alter table "public"."bill_members" drop constraint "bill_members_bill_id_fkey";
-
-drop function if exists "public"."update_updatedat_column"();
-
-alter table "public"."bill_members" drop column "billId";
-
-alter table "public"."bill_members" drop column "createdAt";
-
-alter table "public"."bill_members" drop column "updatedAt";
-
-alter table "public"."bill_members" drop column "userId";
-
-alter table "public"."bill_members" add column "bill_id" uuid not null default gen_random_uuid();
-
-alter table "public"."bill_members" add column "created_at" timestamp with time zone not null default now();
-
-alter table "public"."bill_members" add column "updated_at" timestamp with time zone;
-
-alter table "public"."bill_members" add column "user_id" uuid not null;
-
-alter table "public"."bills" drop column "createdAt";
-
-alter table "public"."bills" drop column "creatorId";
-
-alter table "public"."bills" drop column "issuedAt";
-
-alter table "public"."bills" drop column "updaterId";
-
-alter table "public"."bills" add column "created_at" timestamp with time zone not null default now();
-
-alter table "public"."bills" add column "creator_id" uuid not null;
-
-alter table "public"."bills" add column "issued_at" date not null;
-
-alter table "public"."bills" add column "updater_id" uuid;
-
-alter table "public"."notifications" drop column "billId";
-
-alter table "public"."notifications" drop column "createdAt";
-
-alter table "public"."notifications" drop column "readStatus";
-
-alter table "public"."notifications" drop column "triggerId";
-
-alter table "public"."notifications" drop column "userId";
-
-alter table "public"."notifications" add column "bill_id" uuid;
-
-alter table "public"."notifications" add column "created_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text);
-
-alter table "public"."notifications" add column "read_status" boolean not null default false;
-
-alter table "public"."notifications" add column "trigger_id" uuid not null;
-
-alter table "public"."notifications" add column "user_id" uuid not null default gen_random_uuid();
-
-alter table "public"."profiles" drop column "fullName";
-
-alter table "public"."profiles" drop column "updatedAt";
-
-alter table "public"."profiles" add column "full_name" text not null default '''''''User '''' || gen_random_uuid()''::text'::text;
-
-alter table "public"."profiles" add column "updated_at" timestamp with time zone;
+ALTER TABLE "public"."bill_members" RENAME COLUMN "billId" TO "bill_id";
+ALTER TABLE "public"."bill_members" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "public"."bill_members" RENAME COLUMN "updatedAt" TO "updated_at";
+ALTER TABLE "public"."bill_members" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "public"."bill_members" RENAME COLUMN "userId" TO "user_id";
 
 alter table "public"."bill_members" add constraint "bill_members_user_id_fkey" FOREIGN KEY (user_id) REFERENCES profiles(id) not valid;
-
 alter table "public"."bill_members" validate constraint "bill_members_user_id_fkey";
 
+alter table "public"."bill_members" add constraint "bill_members_bill_id_fkey" FOREIGN KEY (bill_id) REFERENCES bills(id) ON DELETE CASCADE not valid;
+alter table "public"."bill_members" validate constraint "bill_members_bill_id_fkey";
+
+-- bills
+
+ALTER TABLE "public"."bills" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "public"."bills" RENAME COLUMN "issuedAt" TO "issued_at";
+ALTER TABLE "public"."bills" RENAME COLUMN "creatorId" TO "creator_id";
+ALTER TABLE "public"."bills" RENAME COLUMN "updatedAt" TO "updated_at";
+ALTER TABLE "public"."bills" RENAME COLUMN "updaterId" TO "updater_id";
+
 alter table "public"."bills" add constraint "bills_creator_id_fkey" FOREIGN KEY (creator_id) REFERENCES profiles(id) not valid;
-
 alter table "public"."bills" validate constraint "bills_creator_id_fkey";
-
 alter table "public"."bills" add constraint "bills_updater_id_fkey" FOREIGN KEY (updater_id) REFERENCES profiles(id) not valid;
-
 alter table "public"."bills" validate constraint "bills_updater_id_fkey";
 
+ALTER TABLE "public"."notifications" RENAME COLUMN "userId" TO "user_id";
+ALTER TABLE "public"."notifications" RENAME COLUMN "readStatus" TO "read_status";
+ALTER TABLE "public"."notifications" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "public"."notifications" RENAME COLUMN "billId" TO "bill_id";
+ALTER TABLE "public"."notifications" RENAME COLUMN "triggerId" TO "trigger_id";
+
 alter table "public"."notifications" add constraint "notifications_bill_id_fkey" FOREIGN KEY (bill_id) REFERENCES bills(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
 alter table "public"."notifications" validate constraint "notifications_bill_id_fkey";
-
 alter table "public"."notifications" add constraint "notifications_trigger_id_fkey" FOREIGN KEY (trigger_id) REFERENCES profiles(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
 alter table "public"."notifications" validate constraint "notifications_trigger_id_fkey";
-
 alter table "public"."notifications" add constraint "notifications_user_id_fkey" FOREIGN KEY (user_id) REFERENCES profiles(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
 alter table "public"."notifications" validate constraint "notifications_user_id_fkey";
 
-alter table "public"."bill_members" add constraint "bill_members_bill_id_fkey" FOREIGN KEY (bill_id) REFERENCES bills(id) ON DELETE CASCADE not valid;
-
-alter table "public"."bill_members" validate constraint "bill_members_bill_id_fkey";
+-- profiles
+ALTER TABLE "public"."profiles" RENAME COLUMN "updatedAt" TO "updated_at";
+ALTER TABLE "public"."profiles" RENAME COLUMN "fullName" TO "full_name";
 
 set check_function_bodies = off;
 
-CREATE OR REPLACE FUNCTION public.update_updated_at_bill_tbl()
+CREATE OR REPLACE FUNCTION public.update_updated_at()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$BEGIN
