fc4a4d43e76698929a386b9d03ae6ca9d8f6e925
Nam Hoang Le <hoangnamb1.288@gmail.com>
2025-01-29 14:06:34 +0700

    7

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,49 +1,35 @@
-import * as React from "react"
-import { Stack, Circle, Skeleton as ChakraSkeleton } from "@chakra-ui/react"
-import type {
-  CircleProps,
-  SkeletonProps as ChakraSkeletonProps,
-} from "@chakra-ui/react"
+import * as React from "react";
+import { Stack, Circle, Skeleton as ChakraSkeleton } from "@chakra-ui/react";
+import type { CircleProps, SkeletonProps as ChakraSkeletonProps } from "@chakra-ui/react";
 
 export interface SkeletonCircleProps extends ChakraSkeletonProps {
-  size?: CircleProps["size"]
+	size?: CircleProps["size"];
 }
 
-export const SkeletonCircle = React.forwardRef<
-  HTMLDivElement,
-  SkeletonCircleProps
->(function SkeletonCircle(props, ref) {
-  const { size, ...rest } = props
+export const SkeletonCircle = React.forwardRef<HTMLDivElement, SkeletonCircleProps>(function SkeletonCircle(props, ref) {
+	const { size, ...rest } = props;
 
-  return (
-    <Circle asChild ref={ref} size={size}>
-      <ChakraSkeleton {...rest} />
-    </Circle>
-  )
-})
+	return (
+		<Circle asChild ref={ref} size={size}>
+			<ChakraSkeleton {...rest} />
+		</Circle>
+	);
+});
 
 export interface SkeletonTextProps extends ChakraSkeletonProps {
-  noOfLines?: number
+	noOfLines?: number;
 }
 
-export const SkeletonText = React.forwardRef<HTMLDivElement, SkeletonTextProps>(
-  function SkeletonText(props, ref) {
-    const { gap, noOfLines = 3, ...rest } = props
+export const SkeletonText = React.forwardRef<HTMLDivElement, SkeletonTextProps>(function SkeletonText(props, ref) {
+	const { gap, noOfLines = 3, ...rest } = props;
 
-    return (
-      <Stack gap={gap} ref={ref} width="full">
-        {Array.from({ length: noOfLines }).map((_, index) => (
-          <ChakraSkeleton
-            height="4"
-            key={index}
-            {...props}
-            _last={{ maxW: "80%" }}
-            {...rest}
-          />
-        ))}
-      </Stack>
-    )
-  },
-)
+	return (
+		<Stack gap={gap} ref={ref} width="full">
+			{Array.from({ length: noOfLines }).map((_, index) => (
+				<ChakraSkeleton height="4" key={index} {...props} _last={{ maxW: "80%" }} {...rest} />
+			))}
+		</Stack>
+	);
+});
 
-export const Skeleton = ChakraSkeleton
+export const Skeleton = ChakraSkeleton;
