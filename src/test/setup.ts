b8d3744a891ec55e42b38126e360f03d23d53890
Nam Hoang Le <hoangnamb1.288@gmail.com>
2025-02-10 21:30:37 +0700

    2

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
 import base from "@playwright/test";
+import { loadEnvConfig } from "@next/env";
 import { createClient } from "@supabase/supabase-js";
 
 import { type Database } from "@/database.types";
 import { truncate } from "@/test/functions/truncate";
 
-dotenv.config({ path: ".env.local" });
+loadEnvConfig(process.cwd());
 
 export const supabaseTest = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 
