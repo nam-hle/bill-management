0c15c97fba1e78f40161fd41ed993bfca4e81224
Nam Hoang Le <hoangnamb1.288@gmail.com>
2025-03-07 22:35:59 +0700

    1

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 import { Frame } from "@/components/tabs";
+import { GroupSwitcher } from "@/components/group-switcher";
 import { AvatarContainer } from "@/components/avatar-container";
 import { ThemeToggleButton } from "@/components/theme-toggle-button";
 import { NotificationContainer } from "@/components/notification-container";
@@ -12,6 +13,7 @@ export const NavigationBar: React.FC<Partial<AvatarContainer.Props>> = ({ pendin
 		<header className="mx-auto flex max-w-screen-2xl items-center justify-between px-8 py-2">
 			<div className="flex min-h-[48px] items-center gap-2">{pendingUserInfo && <Frame />}</div>
 			<div className="flex min-h-[48px] items-center gap-2">
+				<GroupSwitcher />
 				<ThemeToggleButton />
 				{pendingUserInfo && (
 					<>
