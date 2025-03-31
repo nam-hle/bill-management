afbbfd3bd353b7496d4a785284f40b358175bc8d
Nam Hoang Le <hoangnamb1.288@gmail.com>
2025-03-31 14:48:04 +0700

    Comment sodium

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -12,7 +12,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH .SCHEMA "pg-0sodium";
+-- CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH .SCHEMA "pg-0sodium";
 
 
 
